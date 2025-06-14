1c1458433521373c67f8770d2a2e63e2f82a2999
Kamal kant sahu <sahukamalkant28@gmail.com>
2025-06-14 23:05:30 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="bg-blur fixed w-full top-0 z-50  shadow-md">
-      <div className="container mx-auto px-11 py-3 flex justify-between items-center">
+      <div className="container mx-auto sm:px-11 px-4 py-3 flex justify-between items-center">
         <Link to="/" className="text-xl font-semibold text-white hover:text-blue-400 transition-colors">
           <span className="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
             MyBlog
